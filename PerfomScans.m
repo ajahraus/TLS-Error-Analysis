5d89945aca6a7b0e22e20e55c0ef9fe9a2c4df34
ajahraus <adamjahraus@gmail.com>
2015-11-17 13:21:25 -0700

    Add final function and scan scrpit

---
+++ PerfomScans.m
@@ -0,0 +1,39 @@
+% Scanning Script
+clear
+close all
+clc
+
+newClouds = ScanAndRegisterFromFiles('kuukpak scan models.txt','kuukpak planar model.txt','kuukpak sphere models.txt');
+
+
+%% Plot Clouds in common coodinate system
+figure, hold on
+cols = rand(length(newClouds),3);
+for i = 1:length(newClouds)
+    newClouds(i).plotCloud3D('reg','old',cols(i,:));
+end
+axis equal
+title('Registered Coordinates')
+
+%%
+for i = 1:length(newClouds)
+    disp(['Cloud number: ', num2str(i)])
+    disp('Registration parameters: (x, y, z, omega, phi, kappa)');
+    disp(newClouds(i).scan.regParams(1:3)');
+    disp(newClouds(i).scan.regParams(4:6)');
+    disp('Registration parameter standard deviations: (mm, arcseconds)')
+    disp(sqrt(diag(newClouds(i).scan.regParamVarCovar(1:3,1:3)))'*1000);
+    disp(sqrt(diag(newClouds(i).scan.regParamVarCovar(4:6,4:6)))'*3600*180/pi);
+    disp(' ')
+end
+
+%%
+allVariances = [];
+for i = 1:length(newClouds)
+    allVariances = [allVariances; newClouds(i).varRegXYZ];
+    disp(['Mean Standard Deviation of X, Y, Z post registration (mm) for cloud number ', num2str(i), ':'])
+    disp(mean(sqrt(newClouds(i).varRegXYZ))*1000)
+end
+
+disp('Mean Standard Deviation of X, Y, Z post registration (mm):')
+disp(mean(sqrt(allVariances))*1000)
---
+++ ScanAndRegisterFromFiles.m
@@ -0,0 +1,53 @@
+function newClouds = ScanAndRegisterFromFiles(varargin)
+% newClouds = ScanAndRegisterFromFiles(varargin)
+% This function takes in the file names of the scanners, planes and spheres
+% to be used in a virtual scan and registration. It performs the scan,
+% registers them together, and returns an array of Cloud objects which
+% contain all of the spatial and statistical information of the clouds.
+
+scanFileName = varargin{1};
+planesFileName  = varargin{2};
+spheresFileName = varargin{3};
+
+if length(varargin) > 3
+    homeScanIndex  = varargin{4};
+else
+    homeScanIndex = 1;
+end
+
+scannersRaw = load(scanFileName);
+
+for i = 1:size(scannersRaw,1)
+    scan(i) = Scanner(scannersRaw(i,1:3),scannersRaw(i,4:6));
+%     scan(i).angularIncrement = scan(i).angularIncrement*4;
+    
+    disp(['Scan number ',num2str(i)])
+    tic
+    clouds(i) = scan(i).ScanScene(planesFileName, spheresFileName);
+    toc
+end
+
+[params, C_x] = registerScans(scan(homeScanIndex ), clouds, spheresFileName);
+
+disp('Loading Data...')
+scan = scan(1).updateScans(scan, params, C_x, homeScanIndex);
+
+for i = 1:length(scan)
+    if i ~= homeScanIndex
+        newClouds(i) = Cloud(clouds(i).XYZ,scan(i),'xyz');
+        newClouds(i).varXYZ = clouds(i).varXYZ;
+        newClouds(i).varRTA = clouds(i).varRTA;
+        newClouds(i).errXYZ = clouds(i).errXYZ;
+        newClouds(i).errRTA = clouds(i).errRTA;
+        newClouds(i).varRegXYZ = newClouds(i).propRegErrors();
+    else
+        newClouds(i) = clouds(i);
+        newClouds(i).regXYZ = newClouds(i).XYZ;
+        newClouds(i).varRegXYZ = newClouds(i).varXYZ;
+    end
+    
+end
+disp('Process complete!')
+
+
+end
