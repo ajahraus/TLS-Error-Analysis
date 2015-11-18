262c64a54ae0abdafb1105a98a450fb963cec4c7
ajahraus <adamjahraus@gmail.com>
2015-11-18 15:18:37 -0700

    Modify output slightly
    Just modifying the units used in the output.

--- PerfomScans.m
+++ PerfomScans.m
@@ -18,10 +18,10 @@ title('Registered Coordinates')
 %%
 for i = 1:length(newClouds)
     disp(['Cloud number: ', num2str(i)])
-    disp('Registration parameters: (x, y, z, omega, phi, kappa)');
+    disp('Registration parameters in meters and degrees: (x, y, z, omega, phi, kappa)');
     disp(newClouds(i).scan.regParams(1:3)');
-    disp(newClouds(i).scan.regParams(4:6)');
-    disp('Registration parameter standard deviations: (mm, arcseconds)')
+    disp(newClouds(i).scan.regParams(4:6)'*180/pi);
+    disp('Registration parameter standard deviations: (mm, arc-seconds)')
     disp(sqrt(diag(newClouds(i).scan.regParamVarCovar(1:3,1:3)))'*1000);
     disp(sqrt(diag(newClouds(i).scan.regParamVarCovar(4:6,4:6)))'*3600*180/pi);
     disp(' ')
@@ -35,5 +35,5 @@ for i = 1:length(newClouds)
     disp(mean(sqrt(newClouds(i).varRegXYZ))*1000)
 end
 
-disp('Mean Standard Deviation of X, Y, Z post registration (mm):')
+disp('Mean Standard Deviation of X, Y, Z post registration for all clouds(mm):')
 disp(mean(sqrt(allVariances))*1000)
